5822e94dc128ae9f5a4dd41c4d07c5d58539e222
Avinash Anchal <anchalavinash66@gmail.com>
2021-02-14 09:43:51 +0530

    Validating email exists in DB

--- photoapp-api-users/src/main/java/com/anchal/photoappapiusers/security/WebSecurity.java
+++ photoapp-api-users/src/main/java/com/anchal/photoappapiusers/security/WebSecurity.java
@@ -1,11 +1,14 @@
 package com.anchal.photoappapiusers.security;
 
+import com.anchal.photoappapiusers.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.Environment;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
@@ -13,6 +16,12 @@ public class WebSecurity extends WebSecurityConfigurerAdapter {
 
     @Autowired
     private Environment environment;
+
+    @Autowired
+    private UserService userService;
+
+    @Autowired
+    private BCryptPasswordEncoder bCryptPasswordEncoder;
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
@@ -27,5 +36,10 @@ public class WebSecurity extends WebSecurityConfigurerAdapter {
         AuthFilter authFilter =  new AuthFilter();
         authFilter.setAuthenticationManager(authenticationManager());
         return authFilter;
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userService).passwordEncoder(bCryptPasswordEncoder);
     }
 }
--- photoapp-api-users/src/main/java/com/anchal/photoappapiusers/service/UserService.java
+++ photoapp-api-users/src/main/java/com/anchal/photoappapiusers/service/UserService.java
@@ -6,13 +6,18 @@ import com.anchal.photoappapiusers.repository.UserRepository;
 import org.modelmapper.ModelMapper;
 import org.modelmapper.convention.MatchingStrategies;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.UUID;
 
 @Service
-public class UserService {
+public class UserService implements UserDetailsService {
 
     @Autowired
     private UserRepository userRepository;
@@ -29,5 +34,14 @@ public class UserService {
         userEntity.setEncryptedPassword("Test");userRepository.save(userEntity);
         UserDto userRes = modelMapper.map(userEntity, UserDto.class);
         return userRes;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        UserEntity userEntity = userRepository.findByEmail(username);
+        if(userEntity == null) throw new UsernameNotFoundException(username);
+        return new User(userEntity.getEmail(), userEntity.getEncryptedPassword(),
+                true, true, true, true,
+                new ArrayList<>());
     }
 }
